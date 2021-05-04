921b940768343572e88a1d747fafd26aa67facc0
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-05-04 13:21:58 -0300

    Adds examples and handy scripts

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ Note: requires `nix` + `flakes` working.
 nix build github:ES-Nix/Disaster-Girl-NFT-images-news/6a2e03f85e4fb1f8198ba6e34e47b56185da20ef#network-fetchurl
 cat result
 ```
+
+```bash
+nix develop github:ES-Nix/Disaster-Girl-NFT-images-news/aeb2e7abc6acc6f6835ac4bcc7ac5223145dc714
+```
--- flake.nix
+++ flake.nix
@@ -20,10 +20,38 @@
         '';
 
         wrapp = pkgsAllowUnfree.writeShellScriptBin "wrapp" ''
-          nix build .#ny_times_75
-          nix build .#ny_times_90
-          nix build .#ny_times_100000
-          nix build .#estadao
+          nix build .#ny_times_75 --out-link ny_times_75
+          nix build .#ny_times_90 --out-link ny_times_90
+          nix build .#ny_times_100000 --out-link ny_times_100000
+          nix build .#estadao --out-link estadao
+
+          echo
+          echo "Printing sha256sums:"
+          sha256sum ny_times_75
+          sha256sum ny_times_90
+          sha256sum ny_times_100000
+          sha256sum estadao
+
+          echo
+          echo "Printing file command outputs:"
+          file $(readlink ny_times_75)
+          file $(readlink ny_times_90)
+          file $(readlink ny_times_100000)
+          file $(readlink estadao)
+
+          echo
+          echo "Printing file command outputs:"
+          file $(readlink ny_times_75)
+          file $(readlink ny_times_90)
+          file $(readlink ny_times_100000)
+          file $(readlink estadao)
+        '';
+
+        show_images_files = pkgsAllowUnfree.writeShellScriptBin "show_images_files" ''
+          timeout 5 okular ny_times_75 --presentation
+          timeout 5 okular ny_times_90 --presentation
+          timeout 5 okular ny_times_100000 --presentation
+          timeout 5 okular estadao --presentation
         '';
       in
       {
@@ -55,8 +83,10 @@
             which
             git
             nixpkgs-fmt
+            okular
             python3Minimal
             hook
+            show_images_files
           ];
 
           shellHook = ''
